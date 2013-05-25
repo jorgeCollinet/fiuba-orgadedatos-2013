202f92cbfbb3709125bb5fcd3a0d46f54be46ad0
extramiana <extramiana@gmail.com>
2013-05-25 23:28:32 +0000

    gamma y delta

---
+++ Traductor.h
@@ -0,0 +1,22 @@
+#ifndef TRADUCTOR_H_
+#define TRADUCTOR_H_
+#include <iostream>
+#include <cmath>
+#include "lector_bit.h"
+
+class traductor{
+
+	// lee el siguiente gamma. -1 para error.
+	int read_gamma(LectorBit* reader);
+
+	// escribe 1 int en gamma en el destino.
+	void write_gamma(int num,Escritor_bit* writer);
+
+	// lee el siguiente delta. -1 para error.
+	int read_delta(LectorBit* reader);
+
+	// escribe 1 int en gamma en el destino.
+	void write_delta(int num,Escritor_bit* writer);
+
+};
+#endif
