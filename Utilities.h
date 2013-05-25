a0415315c3d7ece35e2d7072ceabb2e593e2a6be
pablo.gabriel.federico <pablo.gabriel.federico@gmail.com>
2013-05-25 20:20:36 +0000

    agregando fichero .h

---
+++ Utilities.h
@@ -0,0 +1,18 @@
+/*
+ * Utilities.h
+ *
+ *  Created on: 25/05/2013
+ *      Author: pablo
+ */
+
+#ifndef UTILITIES_H_
+#define UTILITIES_H_
+
+
+#include <string.h>
+
+int FoldAdd(char* palabra,int espaciodirecc);
+
+
+
+#endif /* UTILITIES_H_ */
