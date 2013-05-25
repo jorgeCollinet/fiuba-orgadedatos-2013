f7ab692877525865aba9eb6424ce41dc155e62ea
pablo.gabriel.federico@gmail.com <pablo.gabriel.federico@gmail.com@22c23bed-8771-4306-eb1b-38b7c1ffcccf>
2013-05-25 20:19:09 +0000

    Agregue una libreria Utilities, ahi puse una funcion de hashing que habia hecho, pero creo esta liberria para que todos pongamos ahi si quiere funciones o cosas en comun, que pueden servir para varios modulos.

---
+++ trunk/Utilities.cpp
@@ -0,0 +1,40 @@
+/*
+ * Utilities.cpp
+ *
+ *  Created on: 25/05/2013
+ *      Author: pablo
+ */
+
+
+#include <string.h>
+#include <stdio.h>
+#include <ctype.h>
+
+
+/* Function de Hashing. Devuelve numero entre 0 y EspacioDIrecciones-1*/
+
+int FoldAdd(char* palabra,int espaciodirecc){
+
+	int tam = strlen(palabra);
+	int letra1,letra2,i,sumaparcial,sumatotal;
+
+	for(i=0;i<tam;i++){
+
+		letra1=palabra[i];
+		letra2=palabra[i+1];
+
+		sumaparcial= ((letra1+letra2)%espaciodirecc);
+		sumatotal +=sumaparcial;
+
+		if (i+1 == tam) i=tam;
+	}
+
+
+		return (sumatotal%espaciodirecc);
+
+
+
+}
+
+
+
