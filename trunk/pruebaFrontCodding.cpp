746cbbbf3631683fb04b46b6f653f5e84cba7db7
juandausa@gmail.com <juandausa@gmail.com@22c23bed-8771-4306-eb1b-38b7c1ffcccf>
2013-05-27 00:00:27 +0000

    Se agrego el leer palabra. De todas formas hay un pequeño error que no puedo identificar, esta perdiendo algunos bytes y esta haciendo un ivalid read.

--- trunk/pruebaFrontCodding.cpp
+++ trunk/pruebaFrontCodding.cpp
@@ -3,10 +3,28 @@
 
 int main(void){
 	Front_codding* fc = new Front_codding("archivo.dat");
+	fc->modo_escritura();
 	fc->agregar_palabra("perra");
 	fc->agregar_palabra("perros");
 	fc->agregar_palabra("perritos");
 	fc->agregar_palabra("puchache");
 	fc->agregar_palabra("yAhora");
 	delete (fc);
+	Front_codding* fc2 = new Front_codding("archivo.dat");
+	fc2->modo_lectura();
+	char* palabra;
+	// Ojo que la función devuelve un char*, hay que hacer un delete!
+	palabra = fc2 -> leer_proxima_palabra();
+	cout << palabra << std::endl;
+	delete(palabra);
+	palabra = fc2 -> leer_proxima_palabra();
+	cout << palabra << std::endl;
+	delete(palabra);
+	palabra = fc2 -> leer_proxima_palabra();
+	cout << palabra << std::endl;
+	delete(palabra);
+	palabra = fc2 -> leer_proxima_palabra();
+	cout << palabra << std::endl;
+	delete(palabra);
+	delete (fc2);
 };
