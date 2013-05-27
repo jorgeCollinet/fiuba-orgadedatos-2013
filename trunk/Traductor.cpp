2079ad25c23a128b77d2b011bdcf000fc5517425
extramiana@gmail.com <extramiana@gmail.com@22c23bed-8771-4306-eb1b-38b7c1ffcccf>
2013-05-27 00:59:40 +0000

    fix 32132123

--- trunk/Traductor.cpp
+++ trunk/Traductor.cpp
@@ -2,8 +2,9 @@
 #include <iostream>
 #include <cmath>
 #include "lector_bit.h"
+#include "escritor_bit.h"
 
-int traductor::read_gamma(LectorBit* reader){
+int traductor::read_gamma(LectorBit reader){
 
 	int unary = 0;
 	int binary = 0;
@@ -26,7 +27,7 @@ int traductor::read_gamma(LectorBit* reader){
 };
 
 
-void traductor::write_gamma(int num,Escritor_bit* writer){
+void traductor::write_gamma(int num,Escritor_bit writer){
 
 	int unary;
 	int binary;
@@ -39,16 +40,16 @@ void traductor::write_gamma(int num,Escritor_bit* writer){
 
 	// escribo la parte unaria
 	for(int i=1;i<unary;i++){
-		writer.put(1);
+		writer.escribir_bit_desde_abajo(1);
 	};
-	writer.put(0);
+	writer.escribir_bit_desde_abajo(0);
 
 	// escribo la parte binaria
 	for(int i=aux;i>0;i--){
-		aux2 = (int)pow ((float)2, (float)i);
-		if(aux2 <= num){
+		aux2 = (int)pow ((float)2, (float)(i-1));
+		if(aux2 <= binary){
 			writer.escribir_bit_desde_abajo(1);
-			num = num - aux2;
+			binary = binary - aux2;
 		}else{
 			writer.escribir_bit_desde_abajo(0);
 		}
@@ -56,14 +57,13 @@ void traductor::write_gamma(int num,Escritor_bit* writer){
 };
 
 
-int traductor::read_delta(LectorBit* reader){
+int traductor::read_delta(LectorBit reader){
 
 	int gamma = 0;
 	int binary = 0;
-	int aux;
 
 	// decodifico la parte gamma
-	gamma = (this.read_gamma(LectorBit* reader) - 1);
+	gamma = (this->read_gamma(reader) - 1);
 
 	for(int i=gamma;i>0;i--){
 		// decodifico la parte binaria
@@ -76,7 +76,7 @@ int traductor::read_delta(LectorBit* reader){
 }
 
 
-void traductor::write_delta(int num,Escritor_bit* writer){
+void traductor::write_delta(int num,Escritor_bit writer){
 
 	int gamma;
 	int binary;
@@ -88,14 +88,14 @@ void traductor::write_delta(int num,Escritor_bit* writer){
 	binary = num - (int)pow((float)2,(float)aux);
 
 	// escribo la parte gamma
-	this.Write_gamma(gamma,writer);
+	this->write_gamma(gamma,writer);
 
 	// escribo la parte binaria
 	for(int i=aux;i>0;i--){
-		aux2 = (int)pow ((float)2, (float)i);
-		if(aux2 <= num){
+		aux2 = (int)pow ((float)2, (float)(i-1));
+		if(aux2 <= binary){
 			writer.escribir_bit_desde_abajo(1);
-			num = num - aux2;
+			binary = binary - aux2;
 		}else{
 			writer.escribir_bit_desde_abajo(0);
 		}
--- trunk/Traductor.h
+++ trunk/Traductor.h
@@ -1,20 +1,21 @@
 #ifndef TRADUCTOR_H_
 #define TRADUCTOR_H_
-
+#include "lector_bit.h"
+#include "escritor_bit.h"
 
 class traductor{
 
 	// lee el siguiente gamma. -1 para error.
-	int read_gamma(LectorBit* reader);
+	int read_gamma(LectorBit reader);
 
 	// escribe 1 int en gamma en el destino.
-	void write_gamma(int num,Escritor_bit* writer);
+	void write_gamma(int num,Escritor_bit writer);
 
 	// lee el siguiente delta. -1 para error.
-	int read_delta(LectorBit* reader);
+	int read_delta(LectorBit reader);
 
 	// escribe 1 int en gamma en el destino.
-	void write_delta(int num,Escritor_bit* writer);
+	void write_delta(int num,Escritor_bit writer);
 
 };
 #endif
