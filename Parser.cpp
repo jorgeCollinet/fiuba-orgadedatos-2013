751b52fb47dea9be95c1572b26c110feef34fde9
pablo.gabriel.federico <pablo.gabriel.federico@gmail.com>
2013-05-27 16:30:35 +0000

    no esta andando el parser nuevo igualmente lo subo

--- Parser.cpp
+++ Parser.cpp
@@ -9,77 +9,45 @@
 
 using namespace std;
 
-/**Variables de mi parseo */
-
-FILE* fd;
-int documento;
-unsigned int tam=0;
-unsigned int pos=0;
-char * palabra;
-char letra;
-char palabra1[20];
-
-
-/* FUncion que agarra un archivo de texto, extrae los terminos, los guarda en
- * alguna estructura en memoria y.. ( tendria que ordenarla en memoria y grabar
- * a otro txt... ? ).
+/*
+ * Dado un documento de texto, lo parsea (extrae los terminos relevantes)
+ * y los almacena en la estructura dada.
+ *
+ * @param archivoAparsear: es el puntero al archivo a parsear.
+ * @param arbol: es la estructura para guardar las palabras.
+ *
+ * @return true si termino bien, false caso contrario
  */
-void Parser(char* archivo) {
+bool parser(std::string& archivoAparsear){
 
-	fd= fopen(archivo,"r");
+        std::string palabra;
+        const char* invalidos[CANTIDAD_TOTAL_NOPARSE] = {"¡!#$%&'( )*+,-.0123456789:;<=>¿?@[]^_`{|}~/\\\"\n"};
+        ArchivoTexto archivo;
 
-	if (fd == NULL){
+        archivo.abrirParaLectura(archivoAparsear);
+        std::string auxLinea;
 
-		cout<<("\nError de apertura del archivo. \n\n");
-	} else { // Abri mi archivo, listo para ser leido
+        while ( archivo.leerLinea(auxLinea)){
+                char *linea = new char[CANT_MAX];
+                strcpy(linea,auxLinea.c_str());
+                char* auxPalabra = strtok(linea,*invalidos);
 
-		char* buffer;
-		unsigned int ocupacionbuffer;
+                while ( auxPalabra != NULL ){
+                        palabra = auxPalabra;
+                        //Utilidades::pasarAminusculas(palabra);
+                        //trie.insertarPalabra(palabra,direccion);
+                        cout<<palabra<<endl;
 
-
-		while(feof(fd)==0) { // MIentras no se encuentra fin de archivo
-
-			buffer = (char *)(malloc(2000*sizeof(char))); // Asigno memoria para el buffer donde se va a pasar la info del texto
-
-			//Otengo de a parrafos, hasta encontrar /n, y los guardo en buffer.
-			buffer = fgets(buffer,2000,fd); // 2000 es la cantidad de caracteres que leo, debe ser grande para tener todos los de 1 parrafo asi no se corta el parrafo (linea???)
-
-			cout<<buffer;
-			if(buffer == NULL) break;
-			cout<<("TERMINOBUFFER");
-			cout<<("\n");
-			ocupacionbuffer = strlen(buffer);
-			cout<<ocupacionbuffer;
-			cout<<"\n";
-
-			 unsigned int i,j;
-
-			/**Debo Analizar cada parrafo*/
-			for(i=0;i<ocupacionbuffer;i++){
-
-				//Si no encuentro un espacio lo guardo en la palabra1
-				if ((isspace(buffer[i])) == 0) {
-					//Si no es espacio
-					palabra1[tam] = buffer[i];
-					tam++;
-				} else {
-					palabra = (char *)(malloc(tam*sizeof(char)));
-					strcpy(palabra,palabra1);
-					cout<<pos<<documento<<tam<<palabra;
-					free(palabra);
-					pos++;
-					for(j=0;j<tam;j++){
-					palabra1[j]=' ';
-					}
-					tam=0;
-
-				}
-				}
-				free(buffer);
-			}
+                        auxPalabra = strtok (NULL, *invalidos);
+                }
+                delete []linea;
+                delete auxPalabra;
         }
-        fclose(fd);
 
-}
 
+        return true;
+ }
+
+
+#endif
 
