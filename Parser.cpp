4a00c8ad334f405fcfc8ae92f1d13aebbac2fbb6
pablo.gabriel.federico <pablo.gabriel.federico@gmail.com>
2013-05-28 19:15:20 +0000

    contador de palabras en el doc, y pasaje de parametro lista para ir guardando los archivos que fueron parseados

--- Parser.cpp
+++ Parser.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <fstream>
 #include <cstring>
+#include <list>
 
 #ifndef CANTIDAD_TOTAL_NOPARSE
 #define CANTIDAD_TOTAL_NOPARSE 48
@@ -12,12 +13,14 @@ using namespace std;
 /*
  * Dado archivo lo parsea extrayendo las palabras. Lee linea por linea hasta eof
  * usa strtok para eliminar caracteres invalidos
- *
+ * EL 2DO Parametro es donde es la lista donde se va a ir guardando el nombre que parsea para
+ * luego darsela al merge
  *Falta guardaar en estructura...
  *Retorna true si salio  bien
  */
-bool parser(std::string& archivoaParser){
+bool parser(std::string& archivoaParser,list<std::string> mylist){
 
+		int pos=0; // Posicion de la palabra en el documento actual
         std::string palabra;
         const char* carac_invalidos[CANTIDAD_TOTAL_NOPARSE] = {"¡!#$%&'( )*+,-.:;<=>¿?@[]^_`{|}~/\\\"\n"};
         ManejadorArchivos archivo;
@@ -33,8 +36,8 @@ bool parser(std::string& archivoaParser){
                 while ( auxPalabra != NULL ){
                         palabra = auxPalabra;
 
-                        cout<<palabra<<endl;
-
+                        cout<<palabra<<" "<<pos<<endl;
+                        pos++;
                         auxPalabra = strtok (NULL, *carac_invalidos);
                 }
                 delete []linea;
