e8871433c4c5a92d552b55e93ff414963a80b60e
collinetjorge@gmail.com <collinetjorge@gmail.com@22c23bed-8771-4306-eb1b-38b7c1ffcccf>
2013-05-30 18:00:16 +0000

    parser

---
+++ trunk/parser.h
@@ -0,0 +1,15 @@
+
+#ifndef PARSERTEX_H_
+#define PARSERTEX_H_
+
+#include <string.h>
+#include <iostream>
+#include <fstream>
+#include "ManejadorArchivos.h"
+using namespace std;
+
+
+bool parser(std::string& archivoaParser);
+
+
+#endif /* PARSERTEX_H_ */
