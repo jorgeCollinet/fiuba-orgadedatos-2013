0f7f80990c95908d5fc62ee6e9ac7d7dd314a8e0
juandausa <juandausa@gmail.com>
2013-06-02 23:16:52 +0000

    Se corrigieron algunos errores. Ahora ya se puede guardar el 0 en gamma. Lo malo es que corrí la escala en 1. La lectura y escritura de palabras tiene un problema, el último caracter del archivo se lee mal.

--- test_frontCodding.cpp
+++ test_frontCodding.cpp
@@ -1,4 +1,4 @@
-//#define prueba_para_la_estructura_de_front_codding
+#define prueba_para_la_estructura_de_front_codding
 #ifndef prueba_para_la_estructura_de_front_codding
 /*Esto es una prueba para la estructura de front codding, se espera que no pinche(por ahora)*/
 #include "front_codding.h"
@@ -12,10 +12,21 @@ int main(void){
 	}
 	delete (fc);
 	Front_codding* fc2 = new Front_codding("archivo.dat");
+	int res = 0;
 	fc2->modo_lectura();
-	for (int i=0; i < 5; i++)
-		cout << fc2 -> leer_proxima_palabra() << std::endl;
-
+	for (int i=0; i < 5; i++) {
+		string aux = fc2->leer_proxima_palabra();
+		if (palabras[i] != aux) {
+			res = 1;
+			 cout << aux << endl;
+		}
+	}
+	cout << "Guardar y leer palabras en front codding: ";
+	if (res == 1)
+		cout << "incorrecto.";
+	else
+		cout << "correcto.";
 	delete (fc2);
+	cout << endl;
 }
 #endif
