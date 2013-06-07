0d1c0ee63f72712562818233b6fae0a89cbb510c
pablo.gabriel.federico <pablo.gabriel.federico@gmail.com>
2013-06-07 01:22:42 +0000

    funcion que pasa a minusculas los terminos, en libreria utilities

--- Utilities.h
+++ Utilities.h
@@ -8,11 +8,14 @@
 #ifndef UTILITIES_H_
 #define UTILITIES_H_
 
-
+#include <sstream>
+#include <string>
+#include <stdint.h>
+#include <algorithm>
 #include <string.h>
 
 int FoldAdd(char* palabra,int espaciodirecc);
 
-
+static void pasarAminusculas(std::string& str);
 
 #endif /* UTILITIES_H_ */
