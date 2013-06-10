8aa9aa57cabc58d91ce07b48914a4cfe46b26bb3
juandausa <juandausa@gmail.com>
2013-06-10 02:00:12 +0000

    Más pruenas.

--- test_resolvedorConsultas.cpp
+++ test_resolvedorConsultas.cpp
@@ -8,17 +8,26 @@
 #ifndef TESTCONSULTAS
 #include "front_codding.h"
 #include "ResolvedorConsultas.h"
+#include "traductor.h"
 #include <iostream>
 using namespace std;
 
 int main(void) {
-	ResolvedorConsultas rs("archivoFC.dat");
+	ResolvedorConsultas rs("archivo");
 	cout << "Inicio pruebas " << endl;
 	if(rs.cargar_lexico())
 		cout << "Carga de lexico correcta" << endl;
 	else cout << "Carga de lexico incorrecta"<< endl;
 	rs.mostrar_ocurrencias();
-	cout << "Fin pruebas " << endl;
+	//general el indice
+	Traductor unTraductor(WRITE, "archivoDOC.dat");
+	int numero[]= {1,1,1,5,1,1,1,6,3,1,1,4,2,1,4,3,2,4,1,1,2,1,1,1,1,1,1,3,1,1,3,2,1,3,3,1,6,3,1,1,2,2,1,2,3,3,1,1,1};
+	for (unsigned int i=0; i <49; i++ ) {
+		unTraductor.write_delta(numero[i]);
+	}
+	rs.cargar_ocurrencias();
+	rs.mostrar_ocurrencias();
+	cout << "Fin pruebas." << endl;
 	return 0;
 }
 #endif
