b1a1e526cf71f9899d2a3d1e8f22a7c992128012
juandausa@gmail.com <juandausa@gmail.com@22c23bed-8771-4306-eb1b-38b7c1ffcccf>
2013-06-10 16:31:37 +0000

    Màs pruebas, estoy tratando de levantar los offsets y descubrì quie el problema esta en que no se chequea la condiciòn de fin de archivo.

--- trunk/test_resolvedorConsultas.cpp
+++ trunk/test_resolvedorConsultas.cpp
@@ -4,7 +4,7 @@
  *  Created on: 09/06/2013
  *      Author: juan
  */
-//#define TESTCONSULTAS
+#define TESTCONSULTAS
 #ifndef TESTCONSULTAS
 #include "front_codding.h"
 #include "ResolvedorConsultas.h"
@@ -13,21 +13,29 @@
 using namespace std;
 
 int main(void) {
-	ResolvedorConsultas rs("archivo");
+	ResolvedorConsultas* rs = new ResolvedorConsultas("archivo");
 	cout << "Inicio pruebas " << endl;
-	if(rs.cargar_lexico())
+	if(rs->cargar_lexico())
 		cout << "Carga de lexico correcta" << endl;
 	else cout << "Carga de lexico incorrecta"<< endl;
-	rs.mostrar_ocurrencias();
+	rs->mostrar_ocurrencias();
 	//general el indice
-	Traductor unTraductor(WRITE, "archivoDOC.dat");
+	Traductor* unTraductor = new Traductor(WRITE, "archivoDOC.dat");
 	int numero[]= {1,1,1,5,1,1,1,6,3,1,1,4,2,1,4,3,2,4,1,1,2,1,1,1,1,1,1,3,1,1,3,2,1,3,3,1,6,3,1,1,2,2,1,2,3,3,1,1,1};
 	for (unsigned int i=0; i <49; i++ ) {
-		unTraductor.write_delta(numero[i]);
+		unTraductor->write_delta(numero[i]);
 	}
-	rs.cargar_ocurrencias();
-	rs.mostrar_ocurrencias();
+	delete (unTraductor);
+	Traductor otroTraductor(READ, "archivoDOC.dat");
+	while (1) {
+		int numero = otroTraductor.read_delta();
+		if (numero == (-1)) break;
+		cout << numero << ",";
+	}
+	//rs->cargar_ocurrencias();
+	rs->mostrar_ocurrencias();
 	cout << "Fin pruebas." << endl;
+	delete rs;
 	return 0;
 }
 #endif
